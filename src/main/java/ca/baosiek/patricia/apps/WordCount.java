e8d7dc062a5a93a14f178450b646bf2f0f57e129
Bruno Adam Osiek <baosiek@gmail.com>
2020-01-16 12:30:46 -0800

    Including PATRICIATrieNode

--- src/main/java/ca/baosiek/patricia/apps/WordCount.java
+++ src/main/java/ca/baosiek/patricia/apps/WordCount.java
@@ -1,8 +1,24 @@
 package ca.baosiek.patricia.apps;
 
+import ca.baosiek.patricia.nodes.PATRICIATrieNode;
+
 public class WordCount {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!\n");
+
+        PATRICIATrieNode<Integer> node1 = new PATRICIATrieNode<>("Coisuquinha", 1, 1);
+        PATRICIATrieNode<Integer> node2 = new PATRICIATrieNode<>("Hugo", 1, 1);
+        PATRICIATrieNode<Integer> node3 = node1;
+
+        System.out.printf("Node [%s] hash code is: [%d]\n", node1.getKey(), node1.hashCode());
+        System.out.printf("Node [%s] hash code is: [%d]\n", node2.getKey(), node2.hashCode());
+        System.out.printf("Node [%s] hash code is: [%d]\n", node3.getKey(), node3.hashCode());
+
+        node1.setLeft(node2);
+        node1.setRight(node1);
+
+        System.out.println(node1.toString());
+        System.out.println(node2.toString());
+        System.out.println(node3.toString());
     }
 }
